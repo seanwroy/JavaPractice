80ad90d726726ee35d4ceead63383140d4c9b6fe
Sean Roy <39447050+seanwroy@users.noreply.github.com>
2018-08-16 09:46:06 -0400

    Exception Handling
    Put main code in try-catch block for exception handling. Need to make this a habit.

--- Object Arrays/ObjectArray.java
+++ Object Arrays/ObjectArray.java
@@ -1,45 +1,55 @@
+package Arrays;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class ObjectArray {
+public class Exercise6 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-                
-        //Creates an array that contains total number of students
-        System.out.print("Enter number of students: ");
-        int numberOfStudents = input.nextInt();
-        int[] grades = new int[numberOfStudents];
         
-        //Initializes array of student objects with numberOfStudents as the amount
-        Student[] students = new Student[numberOfStudents];
-        for(int i = 0; i < students.length; i++) {
-        	students[i] = new Student();
-        }
-        
-        //Sets each student object's name and grade
-        for(int i = 0; i < students.length; i++) {
-            System.out.print("Enter student #" + i + "'s name: ");
-    		String name = input.next();
-    		students[i].setName(name);
-    		System.out.print("Enter student #" + i + "'s grade: ");
-    		int grade = input.nextInt();
-    		students[i].setGrade(grade);
-        }
-
-        //Sets each student object's grade
-        for(int i = 0; i < students.length; i++) {
-        	int temp = students[i].getGrade();
-        	grades[i] = temp;
-        }
-        
-        //Sets each student object's letter grade
-        int highest = bestGrade(grades);
-        for(int i = 0; i < students.length; i++) {
-        	students[i].setLetterGrade(giveLetterGrade(grades[i], highest));
-        }
-        
-        //Prints each student's name, grade, and letter grade
-        for(int i = 0; i < students.length; i++) {
-        	System.out.println(students[i].toString());
+        //Try block to catch an input mismatch and general exception
+        try {
+	        //Creates an array that contains total number of students
+	        System.out.print("Enter number of students: ");
+	        int numberOfStudents = input.nextInt();
+	        int[] grades = new int[numberOfStudents];
+	        
+	        //Initializes array of student objects with numberOfStudents as the amount
+	        Student[] students = new Student[numberOfStudents];
+	        for(int i = 0; i < students.length; i++) {
+	        	students[i] = new Student();
+	        }
+	        
+	        //Sets each student object's name and grade
+	        for(int i = 0; i < students.length; i++) {
+	            System.out.print("Enter student #" + i + "'s name: ");
+	    		String name = input.next();
+	    		students[i].setName(name);
+	    		System.out.print("Enter student #" + i + "'s grade: ");
+	    		int grade = input.nextInt();
+	    		students[i].setGrade(grade);
+	        }
+	
+	        //Sets each student object's grade
+	        for(int i = 0; i < students.length; i++) {
+	        	int temp = students[i].getGrade();
+	        	grades[i] = temp;
+	        }
+	        
+	        //Sets each student object's letter grade
+	        int highest = bestGrade(grades);
+	        for(int i = 0; i < students.length; i++) {
+	        	students[i].setLetterGrade(giveLetterGrade(grades[i], highest));
+	        }
+	        
+	        //Prints each student's name, grade, and letter grade
+	        for(int i = 0; i < students.length; i++) {
+	        	System.out.println(students[i].toString());
+	        }
+        }catch (InputMismatchException e) {
+        	System.out.println("Use characters only for names and numbers only "
+        			+ "for grades.");
+        }catch (Exception e) {
+        	System.out.println("Error. Please try again.");
         }
         
         input.close();
